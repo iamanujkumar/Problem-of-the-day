b1b1541395a9fd3dcf8ee48fe88877122138909e
Anuj Kumar <147647340+iamanujkumar@users.noreply.github.com>
2023-11-14 22:53:55 +0530

    Add files via upload

---
+++ Check if strings are rotations of each other or not.cpp
@@ -0,0 +1,15 @@
+class Solution
+{
+    public:
+    //Function to check if two strings are rotations of each other or not.
+    bool areRotations(string s1,string s2)
+    {
+        // Your code here
+        if(s1.length()!=s2.length()) return false;
+        string s=s1+s1;
+        
+        return s.find(s2)!=string::npos;
+        
+        
+    }
+};
